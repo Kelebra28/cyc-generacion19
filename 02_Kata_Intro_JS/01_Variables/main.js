c12ea2dcbd7198ed6a79ba5e769423f481b515a3
Kelebra <ricardokelebra@gmail.com>
2022-08-22 19:55:21 -0500

    Intro js

---
+++ 02_Kata_Intro_JS/01_Variables/main.js
@@ -0,0 +1,54 @@
+// alert('Hola mundo desde JS')
+
+// Tipos de datos
+
+// string o texto 
+'Ricardo'
+// number o  numerico
+10
+// boolean o buleano
+true , false
+// arrat o arreglos
+// []
+// object o objeto
+{}
+
+// Variables
+
+var nombre = 'Ricardo'
+var altura = 2
+var programador = true
+
+var num1 = 1000
+var num2 = 500
+
+// oporadores basios
+// + -  / *
+
+var suma =  num1 + num2
+
+
+// Salidas o outputs 
+console.log(num1 + num2)
+console.log('Hola mundo desde la consola')
+console.log(programador)
+console.log(num1 - num2)
+// alert('')
+// consola ->
+
+// Entradas o inputs
+var preguntaNombre = prompt('Ingresa tu nombre')
+console.log(preguntaNombre)
+
+
+// --------------------------Challenge--------------------------
+
+// -> Preguntar al usuario su nombre direccion y telefono
+//    mostrar cada resultado en la  consola :
+// Hola Ricardo: tu direccion es -> con el numero ->
+// -> Pregutar al usuario su edad o  que edad cumplira este año y mostrar en la consola:
+//    que año nacio
+
+
+
+
