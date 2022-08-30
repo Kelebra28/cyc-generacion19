0a713c06ff4afbe6c0056023778ccce1b7a922af
Kelebra <ricardokelebra@gmail.com>
2022-08-29 19:44:22 -0500

    ciclo for

--- 02_Kata_Intro_JS/03_Condicionales/main.js
+++ 02_Kata_Intro_JS/03_Condicionales/main.js
@@ -24,10 +24,60 @@
                     //     console.log('El valor es difernte')
                     // }
 
-var num = 10
+// var num = 10
 
-if(num === 10 && num < 11) {
-    console.log('Verdadero')
+// if(num === 10 && num < 11) {
+//     console.log('Verdadero')
+// }else {
+//     console.log('Falso')
+// }
+
+
+// --------- Challenge 1 -----------
+
+// van a preguntar al usuario un numero y van a poner en la consola
+// el numero y si es par o impar
+// 1: impar
+// 2: par
+// Que pasa si el usuario pone letra x error 
+
+
+// Todo la info que llegue en el promp es *string*
+// var askNumber = Number(prompt('Elige un numero'))
+
+// if(askNumber % 2 === 0) {
+//     console.log(askNumber + ': es par')
+// }else if(askNumber % 2 === 1) {
+//     console.log(askNumber + ': es inpar')
+// }else{
+//     console.error('No es un numero')
+// }
+
+
+//-------- Challenge 2 --------
+// Juego de piedra papel o tijera ✂️ ⛰ 📃
+// Preguntar a 2 usuarios que opcion eleguir
+// Mostrar en consola que jugador gano y porque
+
+
+var player1 = prompt('Elige piedra, papel o tijera')
+var player2 = prompt('Elijre piedra, papel o tijera')
+
+
+if(player1 === player2){
+    console.log('Empate')
+}else if(player1 === 'piedra' && player2 === 'tijeras') {
+        console.log('Gana el jugar 1, piedra gana a tijeras')
+}else if(player1 === 'tijeras' && player2 === 'papel') {
+        console.log('Gana el jugar 1, tijeras gana a papel')
+}else if(player1 === 'papel' && player2 === 'piedra') {
+        console.log('Gana el jugar 1, pepel gana piedra')
+}else if(player2 === 'piedra' && player1 === 'tijeras') {
+        console.log('Gana jugar 2, piedra gana a tijeras')
+}else if(player2 === 'tijeras' && player1 === 'papel') {
+        console.log('Gana el jugar 2, tijeras gana a papel')
+}else if(player2 === 'papel' && player1 === 'piedra') {
+    console.log('Gana el jugar 2, pepel gana piedra')
 }else {
-    console.log('Falso')
+    console.error('No existe ' + player1, player2)
 }
---
+++ 02_Kata_Intro_JS/04_Ciclos/main.js
@@ -0,0 +1,55 @@
+
+// Sitanxis
+// for(inicio; condicion/limite; secuencia){
+
+// }
+
+for(var i = 0; i <= 100; i++){
+    // console.log(i)
+}
+
+
+//Challege
+
+// 100 -> 0 en 1 en 1
+// 0 -> 500 de 50 en 50
+// 100 -> -100 de 10 en 10
+
+//               0        1           2            3            4         5        6
+var animales = ['Leo', 'Escorpion', 'Cocodrilo', 'Mariposa', 'Cuervo', 'Jirafa', 'Hipopotamo']
+
+// console.log(animales)
+// console.log(animales.length)
+for (var x = 0; x < animales.length; x++) {
+    // console.log(animales[x])
+}
+
+//Challege1
+// recorrer los numero 1 - 100 y decir enla consola si son par o impar
+//1: impar
+//2: par
+//3: impar
+// ... 
+
+
+// Challege2
+// FizzBuzz
+// recorrecorer los numeros del 1 - 100
+// si el numero es divisible entre 3 --> '3: fizz'
+// si el numero es divisible entre 5 --> '5: buzz'
+// si el nuermo es divible entre 5 y 3 --> '15: fizzbuzz'
+// '14' 
+//15 -> fiz
+// 15 -> buzz
+
+
+
+// Challege pro  -> Si terminan rapido
+// hacer un tablero de ajedrez 
+
+// ' ' # ' ' # ' ' #
+//  # ' ' # ' ' # ' '
+// ' ' # ' ' # ' ' #
+//  # ' ' # ' ' # ' '
+// ' ' # ' ' # ' ' #
+//  # ' ' # ' ' # ' '
