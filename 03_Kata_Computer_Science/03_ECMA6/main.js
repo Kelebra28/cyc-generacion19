25cf1f321a34bcdb9f405988fb95c1213733f483
Kelebra <ricardokelebra@gmail.com>
2022-10-03 19:26:22 -0500

    ECAMA6

---
+++ 03_Kata_Computer_Science/03_ECMA6/main.js
@@ -0,0 +1,42 @@
+var goblal = 10
+
+
+// function sumar() {
+
+//     let nombre = 'Ricardo'
+//     var local = 6
+//     console.log(local)
+//     console.log(goblal)
+//     console.log(nombre)
+// }
+
+
+// sumar()
+// console.log(goblal)
+
+
+var cambiante = 'xD'
+// console.log(cambiante)
+var cambiante = 10
+// console.log(cambiante)
+cambiante = 100
+// console.log(cambiante)
+
+
+const NOMBRE = 'Ricardo'
+NOMBRE = 'Raul'
+console.log(NOMBRE)
+
+'anitalavalatina'
+
+
+
+function sumaAntes(a, b) {
+    console.log(a + b)
+}
+
+const sumaAhora = (a,b) => {
+    console.log(a + b)
+}
+
+
